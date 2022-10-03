078eb86452ce79ea0eb26ef4df4401c56572516b
IsaiVelasco <velascohernandes.2017@gmail.com>
2022-10-03 15:34:09 -0500

    First commit

---
+++ src/main/java/com/breaktrail/orderservice/BreaktrailApplication.java
@@ -0,0 +1,13 @@
+package com.breaktrail.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreaktrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreaktrailApplication.class, args);
+	}
+
+}
